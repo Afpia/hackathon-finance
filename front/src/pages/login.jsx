8b34ac1a62bd85eb14584e127df893ce1d286501
Nikita Galkin <nikiprostoff@gmail.com>
2024-11-29 23:47:21 +0500

    Добавил ui

--- front/src/pages/login.jsx
+++ front/src/pages/login.jsx
@@ -4,6 +4,7 @@ export const Login = () => {
 	return (
 		<div className='w-[200px]'>
 			<Button variant='outlined'>Outlined</Button>
+			<Button variant='contained'>Outlined</Button>
 		</div>
 	)
 }
