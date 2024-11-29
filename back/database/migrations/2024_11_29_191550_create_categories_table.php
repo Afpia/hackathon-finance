13737e1499c175a7adedfa860e958974245c7f34
daniil1412412 <daniil.kondratev.1337@gmail.com>
2024-11-30 00:25:09 +0500

    modelANDmigration

---
+++ back/database/migrations/2024_11_29_191550_create_categories_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->enum('category_name', ['одежда', 'хобби', 'супермаркет', 'рестораны', 'здоровье', 'транспорт', 'комунальные услуги', 'свзяь', 'другое']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
