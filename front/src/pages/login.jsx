a5b1a2856aad176a109ba897e51fdd4b14eaaa0f
Nikita Galkin <nikiprostoff@gmail.com>
2024-11-29 23:42:50 +0500

    Добавления ui библиотеки

--- front/src/pages/login.jsx
+++ front/src/pages/login.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import { Button } from '@mui/material'
 
 export const Login = () => {
-	return <div>Login</div>
+	return (
+		<div className='w-[200px]'>
+			<Button variant='outlined'>Outlined</Button>
+		</div>
+	)
 }
