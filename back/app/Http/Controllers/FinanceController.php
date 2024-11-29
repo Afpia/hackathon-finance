d6717561726735b993f3fc384a48f0655df3036b
daniil1412412 <daniil.kondratev.1337@gmail.com>
2024-11-30 03:26:12 +0500

    main routes

--- back/app/Http/Controllers/FinanceController.php
+++ back/app/Http/Controllers/FinanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Finance;
 use App\Services\FinanceService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FinanceController extends Controller
 {
@@ -17,14 +18,32 @@ class FinanceController extends Controller
     {
         return response()->json(['finance' => $this->financeService->all()]);
     }
+    public function show($id){
+        
+        return response()->json(['finance' => $this->financeService->find($id)]);
+    }
     public function store(Request $request){
         $request->validate([
             'incomeORexpense' => 'required',
             'description' => 'reqiured',
         ]);
-        Finance::create([
+        $this->financeService->create([
+            'user_id' => Auth::id(),
+            'category_id' => $request->category_id,
             'incomeORexpense' => $request->incomeORexpense,
             'description' => $request->description,
         ]);
+
+    }
+    public function update(Request $request, $id){
+        $this->financeService->update(
+        $id,
+        ['incomeORexpense' => $request->incomeORexpense,
+                'description' => $request->description
+                ]);
+        
+    }
+    public function destroy($id){
+        $this->financeService->destroy($id);
     }
 }
