96ec51fe9a7b2d665a083d09997d98cf5bd70ea7
Nikita Galkin <nikiprostoff@gmail.com>
2024-12-01 00:42:38 +0500

    Добавление выхода

--- front/src/utils/helpers/notification.js
+++ front/src/utils/helpers/notification.js
@@ -1,4 +1,4 @@
 import toast from 'react-hot-toast'
 
-export const notifySuccess = () => toast.success('Вы вошли в систему')
-export const notifyError = (error) => toast.error(`Мы не смогли войти в систему ${error}`)
+export const notifySuccess = (message) => toast.success(message)
+export const notifyError = (error) => toast.error(error)
