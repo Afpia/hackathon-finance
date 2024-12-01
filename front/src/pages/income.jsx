d068b9a15f5a580ac8d7dec3d6225137f55533cc
Nikita Galkin <nikiprostoff@gmail.com>
2024-12-01 16:24:48 +0500

    Добавление дохода

--- front/src/pages/income.jsx
+++ front/src/pages/income.jsx
@@ -1,9 +1,55 @@
 import { Button, Typography, Box, TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+import { useEffect, useState } from 'react'
+import { notifyError, notifySuccess } from '../utils/helpers/notification'
+import { finance } from '../utils/api/request/finance'
+import { useAuth } from '../app/providers/auth/useAuth'
+import { categories } from '../utils/api/request/categories'
 
 export const Income = () => {
+	const { session } = useAuth()
+	const [categoriesList, setCategoriesList] = useState([])
 	const [incomeHistory, setIncomeHistory] = useState([])
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const { data } = await categories({ config: { headers: { Authorization: 'Bearer ' + `${session.accessToken}` } } })
+				setCategoriesList(data)
+			} catch (error) {
+				console.log(error.message)
+			}
+		}
+		fetchData()
+	}, [categoriesList, session.accessToken])
+
+	const formik = useFormik({
+		initialValues: {
+			incomeORexpense: 0,
+			description: '',
+			category_id: 0,
+			type: 'income'
+		},
+		validate: (values) => {
+			const errors = {}
+			Object.keys(values).forEach((key) => {
+				if (!values[key]) {
+					return (errors[key] = 'Обязательное поле')
+				}
+			})
+			return errors
+		},
+		onSubmit: async (values) => {
+			console.log(values)
+			try {
+				await finance({ data: values, config: { headers: { Authorization: 'Bearer ' + `${session.accessToken}` } } })
+				notifySuccess('Вы успешно добавили доход')
+			} catch (error) {
+				notifyError(error.message)
+				formik.setErrors({ email: true, password: true })
+			}
+		}
+	})
 
 	return (
 		<>
@@ -68,17 +114,59 @@ export const Income = () => {
 					<Typography variant='h6' gutterBottom sx={{ color: '#4A90E2', fontWeight: 'bold' }}>
 						Добавить доход
 					</Typography>
-					<form>
-						<TextField label='Сумма' name='amount' fullWidth required sx={{ marginBottom: 2 }} type='number' />
-						<TextField label='Описание' name='description' fullWidth required sx={{ marginBottom: 2 }} />
+					<form onSubmit={formik.handleSubmit}>
+						<TextField
+							id='incomeORexpense'
+							label='Сумма'
+							name='amount'
+							fullWidth
+							sx={{ marginBottom: 2 }}
+							slotProps={{
+								htmlInput: {
+									min: 0
+								}
+							}}
+							type='number'
+							{...formik.getFieldProps('incomeORexpense')}
+							error={!!(formik.touched.incomeORexpense && formik.errors.incomeORexpense)}
+							helperText={
+								formik.touched.incomeORexpense && formik.errors.incomeORexpense ? (
+									<span style={{ color: 'red' }}>{formik.errors.incomeORexpense}</span>
+								) : null
+							}
+						/>
+						<TextField
+							label='Описание'
+							name='description'
+							fullWidth
+							sx={{ marginBottom: 2 }}
+							{...formik.getFieldProps('description')}
+							error={!!(formik.touched.description && formik.errors.description)}
+							helperText={
+								formik.touched.description && formik.errors.description ? (
+									<span style={{ color: 'red' }}>{formik.errors.description}</span>
+								) : null
+							}
+						/>
 						<FormControl fullWidth sx={{ marginBottom: 2 }}>
 							<InputLabel>Категория</InputLabel>
-							<Select name='category' label='Категория' required>
-								<MenuItem value='Salary'>Зарплата</MenuItem>
-								<MenuItem value='Freelance'>Фриланс</MenuItem>
-								<MenuItem value='Investment'>Инвестиции</MenuItem>
-								<MenuItem value='Other'>Прочее</MenuItem>
+							<Select
+								name='category'
+								label='Категория'
+								{...formik.getFieldProps('category_id')}
+								error={!!(formik.touched.category_id && formik.errors.category_id)}
+							>
+								{categoriesList.map((item, index) => (
+									<MenuItem key={index} value={item.id}>
+										{item.category_name}
+									</MenuItem>
+								))}
 							</Select>
+							{formik.touched.category_id && formik.errors.category_id ? (
+								<span style={{ color: 'red', fontSize: '13px', marginLeft: '14px', fontWeight: '400' }}>
+									{formik.errors.category_id}
+								</span>
+							) : null}
 						</FormControl>
 						<Button variant='contained' type='submit' sx={{ width: '100%', marginBottom: 2, padding: '12px' }}>
 							Добавить доход
